04d457ad2ee1801f007a98f9b301f8208ad2e217
shubham_passi_007 <subhamk0@gmail.com>
2020-08-02 10:40:17 +0530

    Responnsive

--- js/main.js
+++ js/main.js
@@ -1,30 +1,64 @@
-$(document).ready(function(){
-    $nav=$('.nav');
-    $toggleCollapse=$('.toggle-collapse');
+const responsive = {
+  0: {
+    items:1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  }
+};
 
-    /** click Event On Toggle Menu */
-    $toggleCollapse.click(function(){
-        $nav.toggleClass('collapse');
-    })
+$(document).ready(function () {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
 
-    //owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-nav-prev'), $('.owl-nav-next')]
+  /** click Event On Toggle Menu */
+  $toggleCollapse.click(function () {
+    $nav.toggleClass("collapse");
+  });
+
+  //owl-crousel for blog
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$(".owl-nav-prev"), $(".owl-nav-next")],
+    responsive: responsive
+  });
+
+  $(".icon-left-right-ka-baap").css({ background: "transparent" });
+
+  //owl-carousel style left and right buttons
+  $(".owl-prev").mouseover(function () {
+    $(this).css({
+      background: "transparent",
+      color: "var(--text-gray)",
+      outline: "none",
     });
+  });
 
-    $('.icon-left-right-ka-baap').css({background:'transparent'});
+  $(".owl-next").mouseover(function () {
+    $(this).css({
+      background: "transparent",
+      color: "var(--text-gray)",
+      outline: "none",
+    });
+  });
 
-    //owl-carousel style left and right buttons
-    $(".owl-prev").mouseover(function(){
-        $(this).css({background:'transparent', color:'var(--text-gray)', outline:'none'});
-    })
-
-    $(".owl-next").mouseover(function(){
-        $(this).css({background:'transparent', color:'var(--text-gray)', outline: 'none'})
-    })
+  //click to scroll top
+  $(".move-up span").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 });
